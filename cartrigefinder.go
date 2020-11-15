781bbdb6f986c12b786bdf2882271ab2711b511f
vsile <godgrace@yandex.ru>
2020-11-15 17:56:01 +0300

    При нажатии на кнопку "Заменить картридж" сообщение направляется автору

--- cartrigefinder.go
+++ cartrigefinder.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cartrigefinder/db"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"strings"
@@ -17,6 +18,18 @@ func getToken() string {
 	return strings.TrimSpace(string(data))
 }
 
+func getCustomerNameById(id int64) string {
+	name := ""
+	for _, customer := range db.Customers {
+		if customer.ID == id {
+			name = customer.Name
+			break
+		}
+	}
+
+	return name
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
@@ -24,12 +37,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	log.Println(db.Customers)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -43,10 +51,19 @@ func main() {
 		//log.Printf("%+v\n", update.Message.Chat)
 		if update.Message.Text == "/start" {
 			log.Println("Человек ввёл start")
-			text := "Здравствуйте, я помогу заменить Вам картридж"
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Здравствуйте, я помогу заменить Вам картридж")
 			//msg.ReplyToMessageID = update.Message.MessageID
 			msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Заменить картридж")))
+			bot.Send(msg)
+		}
+
+		if update.Message.Text == "Заменить картридж" {
+			customerName := getCustomerNameById(update.Message.Chat.ID)
+			text := fmt.Sprintf("Поступила заявка от пользователя %s (ID: %d)", customerName, update.Message.Chat.ID)
+			msg := tgbotapi.NewMessage(295415523, text)
+			bot.Send(msg)
+
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Ваша заявка принята! Ожидайте! Скоро будет! 100%!\n\nДа не волнуйтесь Вы!")
 			bot.Send(msg)
 		}
 
